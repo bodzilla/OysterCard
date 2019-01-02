760c2041d30a416b32696f6ac49275ed693f88ca
Bodrul <bodrul.ic@msn.com>
2019-01-02 17:23:41 +0000

    - Changed look of login/logout. - Fixed navbar height.

--- OysterCard/OysterCard.Core/ViewModels/UserLoginVM.cs
+++ OysterCard/OysterCard.Core/ViewModels/UserLoginVM.cs
@@ -10,14 +10,15 @@ namespace OysterCard.Core.ViewModels
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
+        [Display(Name = "Email", Prompt = "Email")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password", Prompt = "Password")]
         public string Password { get; set; }
 
-        [Display(Name = "Remember me?")]
+        [Display(Name = "Remember me")]
         public bool RememberMe { get; set; }
     }
 }
