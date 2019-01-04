189decabb43f241e6873fa5b5dfb49dc66b81b23
Bodrul <bodrul.ic@msn.com>
2019-01-04 14:47:29 +0000

    - Building home page after login. - Update on formatting. - Fixing footer. - Update on logo.

---
+++ OysterCard/OysterCard.Website/Controllers/OystersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OysterCard.Website.Controllers
+{
+    public class OystersController : Controller
+    {
+        public IActionResult Apply()
+        {
+            return View();
+        }
+    }
+}
