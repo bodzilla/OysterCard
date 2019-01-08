7320ffabbf7ddeca96cd48430de83944498ac429
Bodrul <bodrul.ic@msn.com>
2019-01-08 16:24:34 +0000

    Added some unit tests for settings UOW.

---
+++ OysterCard/OysterCard.UnitTests/UOW/SettingsUOWTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using OysterCard.Core.Contracts.UOW;
+using OysterCard.Core.Models;
+using OysterCard.Persistence;
+using OysterCard.Persistence.UOW;
+using OysterCard.UnitTests.Services;
+
+namespace OysterCard.UnitTests.UOW
+{
+    [TestFixture]
+    public class SettingsUOWTests
+    {
+        private ISettingsUOW _unitOfWork;
+
+        [SetUp]
+        public void Setup() => ServicesInitializer.ConfigureMappings();
+
+        private void SetUpContext()
+        {
+#pragma warning disable 618
+            // Set up in memory context.
+            var options = new DbContextOptionsBuilder<OysterCardContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
+#pragma warning restore 618
+
+            // Assign a mocked db context to the settings unit of work.
+            var context = new OysterCardContext(options);
+            _unitOfWork = new SettingsUOW(context);
+        }
+
+        [TestCase("Key", "Value")]
+        public void AddAsync_AddSettingAsync_ReturnsPass(string key, string value)
+        {
+            SetUpContext();
+
+            // Set up sample data.
+            var data = new Settings { Key = key, Value = value };
+
+            Assert.DoesNotThrowAsync(async () =>
+            {
+                await _unitOfWork.Settings.AddAsync(data);
+                await _unitOfWork.CompleteAsync();
+            });
+        }
+
+        [TestCase("Key", "Value")]
+        public async Task GetAllAsync_GetAllSettingAsync_ReturnsOneSetting(string key, string value)
+        {
+            SetUpContext();
+
+            // Set up sample data.
+            var data = new Settings { Key = key, Value = value };
+
+            await _unitOfWork.Settings.AddAsync(data);
+            await _unitOfWork.CompleteAsync();
+            var settings = await _unitOfWork.Settings.GetAllAsync();
+
+            Assert.That(settings.Count(), Is.EqualTo(1));
+        }
+    }
+}
