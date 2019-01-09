6b15bc69ab2db82ba1398a5717496dbbdb561789
Bodrul <bodrul.ic@msn.com>
2019-01-09 13:50:01 +0000

    Fixed unit test.

--- OysterCard/OysterCard.UnitTests/Common/UtilitiesTests.cs
+++ OysterCard/OysterCard.UnitTests/Common/UtilitiesTests.cs
@@ -14,11 +14,11 @@ namespace OysterCard.UnitTests.Common
         public void Setup() => _utilities = new Utilities();
 
         /// <summary>
-        ///  </summary>
+        /// </summary>
         /// <param name="dateOfBirth">Format: MM/dd/yyyy</param>
         /// <param name="currentDateTime">Format: MM/dd/yyyy</param>
         /// <param name="age"></param>
-        [TestCase("31/12/1994", "01/01/2019", 24)]
+        [TestCase("12/31/1994", "01/01/2019", 24)]
         [TestCase("01/01/2000", "01/01/2019", 19)]
         [TestCase("01/12/2000", "01/01/2019", 18)]
         public void GetAge_CheckCorrectAgeReturned_ReturnsCorrectAge(DateTime dateOfBirth, DateTime currentDateTime, int age)
